39981cf5e0d1b246a30a5bf768e87d58200bd8b7
kukiat <kukiat1996@gmail.com>
2016-12-11 23:59:30 +0700

    marge navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,46 +4,39 @@
     <meta charset="utf-8">
     <title>Suanpalm</title>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
 
-    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
-    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
+      <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
+      <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
+      <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
 
-    <!-- include summernote css/js -->
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.css" rel="stylesheet">
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="{!! asset('select2/dist/js/select2.min.js') !!}"></script>
-    <link rel="stylesheet" href="{!! asset('select2/dist/css/select2.min.css') !!}">
-    <script src="{!! asset('sweetalert/dist/sweetalert.min.js') !!}"></script>
-    <link rel="stylesheet" type="text/css" href="{!! asset('sweetalert/dist/sweetalert.css') !!}">
-    <link href="{!! asset('css/mycss.css') !!}" rel="stylesheet"/>
-    <script type="text/javascript" src="{!! asset('js/myjs.js') !!}"></script>
+
+      <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.css" rel="stylesheet">
+      <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+      <script src="{!! asset('select2/dist/js/select2.min.js') !!}"></script>
+      <link rel="stylesheet" href="{!! asset('select2/dist/css/select2.min.css') !!}">
+      <script src="{!! asset('sweetalert/dist/sweetalert.min.js') !!}"></script>
+      <link rel="stylesheet" type="text/css" href="{!! asset('sweetalert/dist/sweetalert.css') !!}">
+      <link href="{!! asset('css/mycss.css') !!}" rel="stylesheet"/>
+      <script type="text/javascript" src="{!! asset('js/myjs.js') !!}"></script>
+
+<!-- front end team -->
+    <!-- <link rel="stylesheet" type="text/css" href="{!! asset('css/materialize.min.css') !!}">
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/font-awesome.min.css') !!}">
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/classroom.css') !!}">
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/public-components.css') !!}">
+    <script type="text/javascript" src="{!! asset('js/jquery.js') !!}"></script>
+    <script type="text/javascript" src="{!! asset('js/materialize.min.js') !!}"></script>
+    <script type="text/javascript" src="{!! asset('js/public.js') !!}"></script>
+    <script type="text/javascript" src="{!! asset('js/classroom.js') !!}"></script> -->
+<!-- front end team -->
 
   </head>
 <body>
 
-
-
-
-  <!-- <!DOCTYPE html>
-  <html lang="en">
-  <head>
-  	<meta charset="UTF-8">
-  	<title>Classroom</title>
-  	<link rel="stylesheet" type="text/css" href="{!! asset('css/materialize.min.css')!!}">
-  	<link rel="stylesheet" type="text/css" href="{!! asset('css/font-awesome.min.css')!!}">
-  	<link rel="stylesheet" type="text/css" href="{!! asset('css/classroom.css')!!}">
-  	<link rel="stylesheet" type="text/css" href="{!! asset('css/public-components.css')!!}">
-  	<script type="text/javascript" src="{!! asset('js/jquery.min.js')!!}"></script>
-  	<script type="text/javascript" src="{!! asset('js/materialize.min.js')!!}"></script>
-  	<script type="text/javascript" src="{!! asset('js/public.js')!!}"></script>
-  	<script type="text/javascript" src="{!! asset('js/classroom.js')!!}"></script>
-  </head>
-  <body> -->
     @include('layouts.patial.navbar')
     <div class="container main">
       @include('layouts.patial.alert')
